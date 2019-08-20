9a1ba213897b9762f878f744d1b2e34beeced189
Chun-Lin <wulin40063@gmail.com>
2019-08-20 14:30:56 +0800

    fix: 🐛 fix the deps in the effect, data->data.nextPageToken
    move nextPageToken to the state in reducer and fix the deps in the
    effect, data->data.nextPageToken

--- src/components/IntradayChart.jsx
+++ src/components/IntradayChart.jsx
@@ -6,13 +6,34 @@ import dayjs from 'dayjs'
 
 const IntradayChart = () => {
   const intradayFetchReducer = (state, action) => {
-    if (action.type === 'FETCH_INTRADAYPRICE') {
+    if (action.type === 'FETCH_INIT') {
       return {
+        ...state,
+        isError: false,
+        isDone: false,
+      }
+    } else if (action.type === 'FETCH_SUCCESS') {
+      return {
+        ...state,
         intradayPrices: [
           ...state.intradayPrices,
           ...action.payload.intradayPrices,
         ],
         nextPageToken: action.payload.nextPageToken,
+        isError: false,
+        isDone: false,
+      }
+    } else if (action.type === 'FETCH_FAILURE') {
+      return {
+        ...state,
+        isError: true,
+        isDone: false,
+      }
+    } else if (action.type === 'FETCH_DONE') {
+      return {
+        ...state,
+        isError: false,
+        isDone: true,
       }
     } else {
       throw new Error()
@@ -22,38 +43,58 @@ const IntradayChart = () => {
   const [data, dispatch] = useReducer(intradayFetchReducer, {
     intradayPrices: [],
     nextPageToken: '',
+    isError: false,
+    isDone: false,
   })
 
   useEffect(() => {
     const fetchStockIntradayData = async nextPage => {
+      dispatch({ type: 'FETCH_INIT' })
       const queryParameters = {
         symbol: 'MMM',
         pageSize: 100,
-        startDate: '2019-08-15',
-        endDate: '2019-08-15',
+        startDate: '2019-08-19',
+        endDate: '2019-08-19',
         startTime: '13:30:00',
-        endTime: '15:00:00',
+        endTime: '20:00:00',
         nextPage: nextPage,
       }
 
-      const stockIntradayData = await axios.get(
-        `api/securities/intraday?${stringify(queryParameters)}`,
-      )
-      const { intraday_prices, next_page } = stockIntradayData.data
-      dispatch({
-        type: 'FETCH_INTRADAYPRICE',
-        payload: { intradayPrices: intraday_prices, nextPageToken: next_page },
-      })
+      try {
+        const stockIntradayData = await axios.get(
+          `api/securities/intraday?${stringify(queryParameters)}`,
+        )
+        const { intraday_prices, next_page } = stockIntradayData.data
+        dispatch({
+          type: 'FETCH_SUCCESS',
+          payload: {
+            intradayPrices: intraday_prices,
+            nextPageToken: next_page,
+          },
+        })
+        // setNextPageToken(next_page)
+      } catch (error) {
+        dispatch({
+          type: 'FETCH_FAILURE',
+        })
+      }
     }
-
     const { nextPageToken } = data
-    nextPageToken !== null && fetchStockIntradayData(nextPageToken)
-  }, [data, dispatch])
 
-  const { intradayPrices, nextPageToken } = data
+    nextPageToken !== null
+      ? fetchStockIntradayData(nextPageToken)
+      : dispatch({ type: 'FETCH_DONE' })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch, data.nextPageToken])
+
+  const { intradayPrices, isError, isDone } = data
   return (
     <>
-      {nextPageToken === null ? (
+      {isError === true ? <div>Something Wrong!!</div> : null}
+
+      {isDone === false ? (
+        <div>Loading...</div>
+      ) : (
         <VictoryChart scale={{ time: 'time' }}>
           <VictoryAxis dependentAxis />
           <VictoryAxis
@@ -73,7 +114,7 @@ const IntradayChart = () => {
             y="last_price"
           />
         </VictoryChart>
-      ) : null}
+      )}
     </>
   )
 }
